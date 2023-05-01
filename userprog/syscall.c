ba3762eac54594957468426c1a0c53df98ae56f3
jy604 <jeongyoungp@gmail.com>
2023-05-01 12:29:32 +0000

    check_address, halt, exit, create, remove 구현

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -24,6 +24,19 @@ void syscall_handler (struct intr_frame *);
 #define MSR_LSTAR 0xc0000082        /* Long mode SYSCALL target */
 #define MSR_SYSCALL_MASK 0xc0000084 /* Mask for the eflags */
 
+// 과제 2 : 주소 유효성 검사 함수
+void check_address(void *addr) {
+	// kernel이 vm에게 못가게, 할당된 page가 존재하도록 유효성 검사 시행
+	// 유저영역이 아니거나, 빈공간에 접근했거나, 
+	// 포인터가 유저 주소를 가리키고 있지만, 아직 페이지로 할당되지 않은 공간에 접근했다면
+	// 프로세스 즉시 종료
+	struct thread *curr = thread_current();
+	if (!is_user_vaddr(addr) || addr == NULL || pml4_get_page(curr->pml4, addr) == NULL)
+		{
+			exit(-1);
+		} 
+}
+
 void
 syscall_init (void) {
 	write_msr(MSR_STAR, ((uint64_t)SEL_UCSEG - 0x10) << 48  |
@@ -41,6 +54,190 @@ syscall_init (void) {
 void
 syscall_handler (struct intr_frame *f UNUSED) {
 	// TODO: Your implementation goes here.
-	printf ("system call!\n");
-	thread_exit ();
+	// printf ("system call!\n"); // printf는 write call을 부름, 지우고 시작 
+	// thread_exit ();
+	int sys_number = f->R.rax; // syscall 번호는 rax에 저장되어있음
+	    /* 
+	인자 들어오는 순서:
+	1번째 인자: %rdi
+	2번째 인자: %rsi
+	3번째 인자: %rdx
+	4번째 인자: %r10
+	5번째 인자: %r8
+	6번째 인자: %r9 
+	*/
+	
+	switch (sys_number)
+	{
+	case SYS_HALT:
+			halt();
+			break;
+	case SYS_EXIT:
+			exit(f->R.rdi);
+			break;
+	case SYS_FORK:
+			fork(f->R.rdi);
+			break;
+	case SYS_EXEC:
+			exec(f->R.rdi);
+			break;
+	case SYS_WAIT:
+			wait(f->R.rdi);
+			break;
+	case SYS_CREATE:
+			create(f->R.rdi, f->R.rsi);
+			break;
+	case SYS_REMOVE:
+			remove(f->R.rdi);
+			break;
+	case SYS_OPEN:
+			open(f->R.rdi);
+			break;
+	case SYS_FILESIZE:
+			filesize(f->R.rdi);
+			break;
+	case SYS_READ:
+			read(f->R.rdi, f->R.rsi, f->R.rdx);
+			break;
+	case SYS_WRITE:
+			write(f->R.rdi, f->R.rsi, f->R.rdx);
+			break;
+	case SYS_SEEK:
+			seek(f->R.rdi, f->R.rsi);
+			break;
+	case SYS_TELL:
+			tell(f->R.rdi);
+	case SYS_CLOSE:
+			close(f->R.rdi);
+	}
+}
+
+void
+halt (void) {
+	power_off();
+}
+
+// 현재 프로세스만 종료하는 call
+void
+exit (int status) {
+	struct thread *curr = thread_current();
+	printf("%s : exit(%d)\n", curr->name, status);
+	thread_exit();
+}
+
+pid_t
+fork (const char *thread_name){
+	return (pid_t) syscall1 (SYS_FORK, thread_name);
+}
+
+int
+exec (const char *file) {
+	return (pid_t) syscall1 (SYS_EXEC, file);
+}
+
+int
+wait (pid_t pid) {
+	return syscall1 (SYS_WAIT, pid);
+}
+
+// 파일을 생성하는 syscall
+bool
+create (const char *file, unsigned initial_size) {
+
+	return syscall2 (SYS_CREATE, file, initial_size);
+}
+
+bool
+remove (const char *file) {
+	return syscall1 (SYS_REMOVE, file);
+}
+
+int
+open (const char *file) {
+	return syscall1 (SYS_OPEN, file);
+}
+
+int
+filesize (int fd) {
+	return syscall1 (SYS_FILESIZE, fd);
+}
+
+int
+read (int fd, void *buffer, unsigned size) {
+	return syscall3 (SYS_READ, fd, buffer, size);
+}
+
+int
+write (int fd, const void *buffer, unsigned size) {
+	return syscall3 (SYS_WRITE, fd, buffer, size);
+}
+
+void
+seek (int fd, unsigned position) {
+	syscall2 (SYS_SEEK, fd, position);
+}
+
+unsigned
+tell (int fd) {
+	return syscall1 (SYS_TELL, fd);
+}
+
+void
+close (int fd) {
+	syscall1 (SYS_CLOSE, fd);
+}
+
+int
+dup2 (int oldfd, int newfd){
+	return syscall2 (SYS_DUP2, oldfd, newfd);
+}
+
+void *
+mmap (void *addr, size_t length, int writable, int fd, off_t offset) {
+	return (void *) syscall5 (SYS_MMAP, addr, length, writable, fd, offset);
+}
+
+void
+munmap (void *addr) {
+	syscall1 (SYS_MUNMAP, addr);
+}
+
+bool
+chdir (const char *dir) {
+	return syscall1 (SYS_CHDIR, dir);
+}
+
+bool
+mkdir (const char *dir) {
+	return syscall1 (SYS_MKDIR, dir);
+}
+
+bool
+readdir (int fd, char name[READDIR_MAX_LEN + 1]) {
+	return syscall2 (SYS_READDIR, fd, name);
+}
+
+bool
+isdir (int fd) {
+	return syscall1 (SYS_ISDIR, fd);
+}
+
+int
+inumber (int fd) {
+	return syscall1 (SYS_INUMBER, fd);
+}
+
+int
+symlink (const char* target, const char* linkpath) {
+	return syscall2 (SYS_SYMLINK, target, linkpath);
+}
+
+int
+mount (const char *path, int chan_no, int dev_no) {
+	return syscall3 (SYS_MOUNT, path, chan_no, dev_no);
+}
+
+int
+umount (const char *path) {
+	return syscall1 (SYS_UMOUNT, path);
 }
